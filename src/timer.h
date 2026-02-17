b3ca15cea4d03ce44379a1bed148643fc5a90379
Oliver Bilbie <68697462+Oliver-Bilbie@users.noreply.github.com>
2026-02-17 14:33:10 -0600

    Fixed timer types on macos

--- src/timer.h
+++ src/timer.h
@@ -5,27 +5,25 @@
 
 class Timer {
  public:
-  Timer() : cycle_start(std::chrono::high_resolution_clock::now()) {};
+  Timer() : cycle_start(std::chrono::system_clock::now()) {};
 
-  void set(std::chrono::time_point<std::chrono::high_resolution_clock> t) {
-    cycle_start = t;
-  };
+  void set(std::chrono::time_point<std::chrono::system_clock> t) { cycle_start = t; };
 
   void set(uint64_t timestamp) {
     cycle_start = std::chrono::system_clock::from_time_t(0) +
                   std::chrono::milliseconds(timestamp);
   }
 
-  void reset() { cycle_start = std::chrono::high_resolution_clock::now(); };
+  void reset() { cycle_start = std::chrono::system_clock::now(); };
 
   long long elapsed() {
     return std::chrono::duration_cast<std::chrono::milliseconds>(
-               std::chrono::high_resolution_clock::now() - cycle_start)
+               std::chrono::system_clock::now() - cycle_start)
         .count();
   };
 
  private:
-  std::chrono::time_point<std::chrono::high_resolution_clock> cycle_start;
+  std::chrono::time_point<std::chrono::system_clock> cycle_start;
 };
 
 #endif
